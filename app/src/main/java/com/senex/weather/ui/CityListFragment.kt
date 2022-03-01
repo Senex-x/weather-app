7c9a6aed6618851c3f89c0476b0a5f9b70c522de
V-Senex <junior.07.inator@gmail.com>
2022-03-01 20:07:34 +0300

    Add Navigation component

---
+++ app/src/main/java/com/senex/weather/ui/CityListFragment.kt
@@ -0,0 +1,30 @@
+package com.senex.weather.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.senex.weather.databinding.FragmentCityListBinding
+import com.senex.weather.databinding.FragmentWeatherBinding
+
+class CityListFragment: Fragment() {
+    private var _binding: FragmentCityListBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) = FragmentCityListBinding.inflate(
+        inflater, container, false
+    ).also { _binding = it }.root
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) = with(binding) {
+
+    }
+}
---
+++ app/src/main/java/com/senex/weather/ui/WeatherFragment.kt
@@ -0,0 +1,29 @@
+package com.senex.weather.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.senex.weather.databinding.FragmentWeatherBinding
+
+class WeatherFragment : Fragment() {
+    private var _binding: FragmentWeatherBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) = FragmentWeatherBinding.inflate(
+        inflater, container, false
+    ).also { _binding = it }.root
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) = with(binding) {
+
+    }
+}
