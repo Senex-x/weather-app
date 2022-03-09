bcc17e5ec7c215ffef66774f9aefde40b4718664
V-Senex <junior.07.inator@gmail.com>
2022-03-09 19:23:45 +0300

    Add temperature level handling

--- app/src/main/java/com/senex/weather/ui/cities/recycler/CityRecyclerAdapter.kt
+++ app/src/main/java/com/senex/weather/ui/cities/recycler/CityRecyclerAdapter.kt
@@ -1,11 +1,14 @@
 package com.senex.weather.ui.cities.recycler
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.senex.weather.common.TemperatureLevel
 import com.senex.weather.common.log
 import com.senex.weather.data.entities.CityInfo
 import com.senex.weather.databinding.ListItemCityWeatherBinding
+import kotlin.math.roundToInt
 
 class CityRecyclerAdapter(
     private val items: List<CityInfo>,
@@ -18,6 +21,10 @@ class CityRecyclerAdapter(
     ) {
         fun bind(item: CityInfo) = with(binding) {
             cityName.text = item.name
+
+            val color = TemperatureLevel.getLevel(item.main.temp.roundToInt()).color
+            val context = root.context
+            temperature.setTextColor(context.resources.getColor(color, context.theme))
             temperature.text = item.main.temp.toString()
         }
     }
