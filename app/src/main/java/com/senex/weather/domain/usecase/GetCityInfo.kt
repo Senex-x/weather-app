008770fc852989b5c93a7bb5fe121d3a78539aa7
V-Senex <junior.07.inator@gmail.com>
2022-04-17 14:57:35 +0300

    Enable constructor injection

--- app/src/main/java/com/senex/weather/domain/usecase/GetCityInfo.kt
+++ app/src/main/java/com/senex/weather/domain/usecase/GetCityInfo.kt
@@ -3,8 +3,9 @@ package com.senex.weather.domain.usecase
 import com.senex.weather.domain.util.Latitude
 import com.senex.weather.domain.util.Longitude
 import com.senex.weather.domain.repository.CityInfoRepository
+import javax.inject.Inject
 
-class GetCityInfo(
+class GetCityInfo @Inject constructor(
     private val weatherRepository: CityInfoRepository
 ) {
     suspend operator fun invoke(
